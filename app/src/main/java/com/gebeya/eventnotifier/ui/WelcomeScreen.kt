0e695903e5414a93e260551c0ad4c1dfdf012f9c
Lishaneticha <33592510+Lishaneticha@users.noreply.github.com>
2024-01-18 10:25:49 +0300

    Google map
    Google map

--- app/src/main/java/com/gebeya/eventnotifier/ui/WelcomeScreen.kt
+++ app/src/main/java/com/gebeya/eventnotifier/ui/WelcomeScreen.kt
@@ -4,23 +4,31 @@ import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -28,6 +36,16 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.gebeya.eventnotifier.ui.components.EventDatePicker
 import com.gebeya.eventnotifier.viewmodel.WelcomeScreenViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import java.time.Instant
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,74 +61,76 @@ fun WelcomeScreen(
 
     println("location: ${location.value?.latitude} ${location.value?.longitude}")
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        val context = LocalContext.current
-        Text(text = "${second}")
 
-        EventDatePicker(
-            eventDate = {
-                Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
+    val context = LocalContext.current
+    val mapType = remember { mutableStateOf(MapType.TERRAIN) }
+
+    if(location.value != null){
+        Box(modifier = Modifier.fillMaxSize()){
+            val cameraPosition = rememberCameraPositionState()
+
+            LaunchedEffect(location.value) {
+                cameraPosition.centerCamera(location.value ?: LatLng(0.0, 0.0))
             }
-        )
 
-        EventDatePicker(
-            eventDate = {
-                Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
-            }
-        )
-
-        val isExpanded = remember{
-            mutableStateOf(false)
-        }
-
-        val eventType = remember {
-            mutableStateOf("")
-        }
-
-
-        val eventTypes = listOf("Conert", "Art Gallery", "Talent show")
-
-        ExposedDropdownMenuBox(
-            expanded = isExpanded.value,
-            onExpandedChange = {
-                isExpanded.value = it
-            }
-        ) {
-            TextField(
-                value = eventType.value,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded.value) },
-                placeholder = { Text(text = "select event type")},
-                modifier = Modifier.menuAnchor()
-            )
-
-            ExposedDropdownMenu(
-                expanded = isExpanded.value,
-                onDismissRequest = { isExpanded.value = false }
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPosition,
+                properties = MapProperties(
+                    mapType = mapType.value, isTrafficEnabled = true, isMyLocationEnabled = true
+                )
             ) {
-                eventTypes.forEach{
-                    DropdownMenuItem(
-                        text = { Text(text = it) },
-                        onClick = {
-                            isExpanded.value = false
-                            eventType.value = it
-                        }
+                Marker(state = MarkerState(location.value ?: LatLng(0.0, 0.0)),
+                    title = "My position",
+                    snippet = "This is my current location updated every 10 sec",
+                    draggable = true,
+                    onClick = {
+                        println("Marker location: ${it.position}")
+                        true
+                    })
+
+                Marker(
+                    state = MarkerState(LatLng(8.997677611550133, 38.76446820795536)),
+                    title = "FLamingo Cafe",
+                    snippet = "This is a cafe near you"
+                )
+
+            }
+            Column(
+                modifier = Modifier.align(Alignment.CenterStart),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = "${second}", color = Color.Black)
+                Button(onClick = {
+                    if (mapType.value == MapType.TERRAIN) {
+                        mapType.value = MapType.SATELLITE
+                    } else {
+                        mapType.value = MapType.TERRAIN
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "",
+                        tint = if(mapType.value == MapType.TERRAIN){ Color.Black } else{ Color.White }
                     )
                 }
             }
-
-
-
         }
-
-        Button(onClick = { navToHomeScreen() }) {
-            Text(text = "continue")
-        }
+    }else{
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){ CircularProgressIndicator() }
     }
+
+}
+
+suspend fun CameraPositionState.centerCamera( location: LatLng ){
+    return animate(
+        update = CameraUpdateFactory.newLatLngZoom( location, 15f ),
+        durationMs = 2000
+    )
 }
 
 fun check(): Boolean {
